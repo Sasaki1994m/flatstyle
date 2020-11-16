02d83430b760d8ed0d0c77ca4c2298ad5e96220c
ryota <ryota@example.com>
2020-11-16 14:57:48 +0000

    コードの整形

--- resources/views/commons/footer.blade.php
+++ resources/views/commons/footer.blade.php
@@ -1,7 +1,7 @@
  <footer class="fotter">
      
         <nav class="navbar navbar-expand-md  navbar-dark bg-primary">
-        <div class="container-fluid justify-content-end">     
+            <div class="container-fluid justify-content-end">     
                 <div class="navbar-brand">© 2020 flatstyle, All rights reserved.</div>
             </div>  
         </nav>
